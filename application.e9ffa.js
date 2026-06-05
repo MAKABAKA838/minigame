81c870e279324d48df59bd070f648cff84cfdc5f
ikizarkovic805-lang <ikizarkovic805@gmail.com>
2026-06-05 16:20:35 +0800

    jian

--- application.e9ffa.js
+++ application.e9ffa.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.9c149.json';
+          this.settingsPath = 'src/settings.c5c05.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
