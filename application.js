982d2bd2bdcd26f47d7a496cdb27a74ad3bd8b44
ikizarkovic805-lang <ikizarkovic805@gmail.com>
2026-06-05 14:34:48 +0800

    yuanxing

--- application.js
+++ application.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.ad6b6.json';
+          this.settingsPath = 'src/settings.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
